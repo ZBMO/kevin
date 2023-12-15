14ce975a445d952dd3f187c96e6c94d3fcb2ea61
zach <zachariah.eb@protonmail.com>
2023-12-15 16:24:06 -0600

    new components

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
 import streamsync as ss
 print("Hello kevin!")
 
-def click_print_payload(payload):
-    if (payload == None): 
-        print("no payload found")
-    else:
-        print("click payload")
-        print(payload)
+def click_handler(payload):
+    print("click payload")
+    print(payload)
 
-def input_print_payload(state, payload):
-    if (payload == None): 
-        print("no payload found")
-    else:
-        print("input payload")
-        print(payload)
-        state["input_value"] = payload["value"]
+def change_handler(state, payload):
+    print("select payload")
+    print(payload)
 
-def select_print_payload(state, payload):
-    if (payload == None): 
-        print("no payload found")
-    else:
-        print("select payload")
-        print(payload)
-        state["selector_value"] = payload["value"]
 
 initial_state = ss.init_state({
     "my_app": {
         "title": "My App"
     },
-    "input_value": None,
-    "selector_value": None,
 })
